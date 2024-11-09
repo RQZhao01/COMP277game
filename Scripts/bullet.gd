2db3ca1351c91ffc9870ed08d667d34332255425
Gaaaaaary <117378427+ghubyl@users.noreply.github.com>
2024-11-09 14:51:39 -0800

    bullet collision fix?

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 var direction_shot: Vector2
 var speed = 3000
@@ -14,13 +14,31 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	self.position = position + direction_shot * speed * delta
+	
+	velocity = direction_shot * speed * delta
+	var collision_detect=move_and_collide(velocity)
+	if collision_detect:
+		print(collision_detect.get_collider())
+		print(collision_detect.get_collider_id())
+		print(collision_detect.get_class())
+		queue_free()
+	
 	
 	
 	pass
 
 
 func _on_area_entered(area: Area2D) -> void:
+	print("111111111111111111111111111111111")
 	if area.is_in_group('Walls'):
+		print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 		queue_free()
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("")
+	print("mmmmmmmmmmmmmmmmmm")
+	print(area)
+	print("")
 	pass # Replace with function body.
---
+++ Scripts/walls.gd
@@ -0,0 +1,12 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	tile_set.set_physics_layer_collision_layer(0,3)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
