eba8c0fab1e0013a405d9d44dc6f774db9262df5
Gaaaaaary <117378427+ghubyl@users.noreply.github.com>
2024-11-21 11:07:07 -0800

    zombie finished
    finished

--- zombie.gd
+++ zombie.gd
@@ -13,49 +13,64 @@ var health = 10
 var pursue = false
 var attack = false
 var anim
+var dead = false
+var count = 0
+var damage = false
+
 
 
 func _ready() -> void:
 	$Sprite2D.play("idle")
 
-	
-		
-		
-
-
 
 func _physics_process(_delta: float) -> void:
-	if health == 0:
-		queue_free()
+	
+	if dead == true:
+		z_index = 0
+		$CollisionShape2D.disabled = true
+		dead = null
+		health = 999
+		velocity = Vector2(0,0)
+		print("dead")
+		$Sprite2D.play("dying1")
+		print("000000")
+	
+	if dead == null:
+		count += 1
+		if count > 150:
+			queue_free()
+		pass
+	
+	elif health == 0 or health <= 0:
+		dead = true
 			#collision_detect.get_collider()
-			
+	elif attack:
 		
-
-	if attack:
-		print("attack")
 		$Sprite2D.play(anim)
+		if damage == true:
+			print("player is hurt")
+			damage = false
 	elif pursue:
 		move_speed = 150
 		$Sprite2D.play("run")
 		navigation_agent_2d.target_position = target.global_position
 	else:
 		if velocity == Vector2(0,0):
-			print("idle")
 			$Sprite2D.play("idle")
 		else:
-			print("walk...")
 			$Sprite2D.play("walk1")
 			move_speed = 100
 		
 	if navigation_agent_2d.is_navigation_finished():
 		velocity = Vector2(0,0)
 		return
+	if health > 0 and health < 100:
+		var current_agent_position = global_position
+		var next_path_position = navigation_agent_2d.get_next_path_position()
+		velocity = current_agent_position.direction_to(next_path_position) * move_speed
 	
-	var current_agent_position = global_position
-	var next_path_position = navigation_agent_2d.get_next_path_position()
-	velocity = current_agent_position.direction_to(next_path_position) * move_speed
+		self.rotation = -velocity.angle_to(Vector2(-1,0)) + PI
 	
-	self.rotation = -velocity.angle_to(Vector2(-1,0)) + PI
 	
 	
 	
@@ -71,7 +86,6 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "Noisecircle":
 		pursue = true
 		navigation_agent_2d.target_position = target.global_position
-		print("entered")
 	
 	
 	pass # Replace with function body.
@@ -100,4 +114,10 @@ func _on_attack_zone_area_entered(area: Area2D) -> void:
 func _on_attack_zone_area_exited(area: Area2D) -> void:
 	if area.name == "hitbox":
 		attack = false
+	pass # Replace with function body.
+
+
+
+func _on_sprite_2d_animation_finished() -> void:
+	damage = true
 	pass # Replace with function body.
