40358ae28a1600f5b453bb6ab30071a25598e8b1
mtulley <mftulley@gmail.com>
2024-12-04 19:15:45 -0800

    fixed

--- Scripts/exit.gd
+++ Scripts/exit.gd
@@ -1,9 +1,9 @@
-extends Panel
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
